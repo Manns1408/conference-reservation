6b84ee78c37e43e49f00ade891c9e27b8e30ad8a
Manns1408 <Mannatsareen1998@gmail.com>
2025-06-16 00:26:41 +1200

    Fix vercel config and index for deployment

--- api/index.py
+++ api/index.py
@@ -1,14 +1,13 @@
-import sys
 import os
+import sys
 from django.core.wsgi import get_wsgi_application
 
-# Set settings path
+# Set project path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Assignment1.settings")
 
-# Load application
-application = get_wsgi_application()
+# WSGI app for Vercel
+app = get_wsgi_application()
 
-# Vercel will call this
 def handler(environ, start_response):
-    return application(environ, start_response)
+    return app(environ, start_response)
